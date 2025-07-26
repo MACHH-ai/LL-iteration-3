2cd829e14389ea0851c954187a9bf7f80f93a3e9
MACHH-ai <jacksonsessil@gmail.com>
2025-07-26 12:25:42 +0300

    Updated _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -65,6 +65,15 @@ export default function TabLayout() {
           ),
         }}
       />
+      <Tabs.Screen
+        name="todo"
+        options={{
+          title: 'Todo',
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="checkmark-done" size={size} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
